2792c44bf514fac224c22a5ece8d536f2b2e32f2
liminyu <yulimin@yulimintekiMacBook-Air.local>
2015-09-01 14:49:56 -0400

    modified:   omega_star.m         add density(x) for omega_star

--- omega_star.m
+++ omega_star.m
@@ -1,5 +1,5 @@
 function f=omega_satr(epsilon,x)
 global R0 a rhoh
 m=1.0; % poloidal mode number
-f=m*dFdE(epsilon)./denp(x)/2.0.*R0/a*rhoh/a;
+f=m*dFdE(epsilon).*density(x)./denp(x)/2.0.*R0/a*rhoh/a;
 end
