513c073d56a15c140127f27b3264a00af9c7df2b
liminyu <yulimin@yulimintekiMacBook-Air.local>
2015-10-30 09:31:49 +0800

    modified:   FL.m

--- test/FL.m
+++ test/FL.m
@@ -1,5 +1,5 @@
 function f=FL(Lambda) % distribution of Lambda component
 global Lambda0 deltaL
-Lambda1=(Lambda-Lambda0)./deltaL.^0.2;
-f=exp(Lambda1.^2);
+Lambda1=(Lambda-Lambda0)./deltaL;
+f=exp(-Lambda1.^2);
 end
